994d2bbd23edbce208b3bdc27d914d2e1f614e1a
g <gongzhr3@163.com>
2026-03-12 11:04:58 +0800

    add type

--- base/scheme_type.h
+++ base/scheme_type.h
@@ -8,6 +8,7 @@ enum class DifferenceSchemeType : std::uint8_t
     Conv_Center2nd_Diff_Center2nd,
     Conv_Upwind1st_Diff_Center2nd,
     Conv_QUICK_Diff_Center2nd,
+    Conv_TVD_VanLeer_Diff_Center2nd,
 };
 
 std::ostream& operator<<(std::ostream& os, DifferenceSchemeType type);
